a5ef62faad1214940bab7e13eb4f6281b679717e
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-21 21:44:42 +0900

    feat: state와 ref 랜더링 차이 비교

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useState, useRef} from "react";
 
 function App() {
   const [count, setCount] = useState(0)
+    const countRef=useRef(0)
 
+    console.log(countRef)
   const increaseCountState = () => {
     setCount(count+1)
   }
+    const increaseCountRef=()=>{
+      countRef.current = countRef.current+1
+    }
+
 
   return (
    <div>
      <p>State:{count}</p>
+       <p>Ref: {countRef.current} </p>
      <button onClick={increaseCountState}>State 올려</button>
+       <button onClick={increaseCountRef}>Ref 올려</button>
    </div>
   );
 }
