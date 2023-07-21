bbce63b2f9eaed54298b3a36ecce6232ee004535
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-21 21:26:20 +0900

    feat: 버튼 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increaseCountState = () => {
+    setCount(count+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <p>State:{count}</p>
+     <button onClick={increaseCountState}>State 올려</button>
+   </div>
   );
 }
 
