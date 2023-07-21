d48eba507f6a4e9f02c61d31023ce48b029001e0
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-21 21:56:19 +0900

    feat: ref var 차이 비교

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import './App.css';
 import {useState, useRef} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-    const countRef=useRef(0)
+    const [renderer, setRenderer] = useState(0)
+    const countRef = useRef(0)
+    let countVar = 0
 
-    console.log(countRef)
-  const increaseCountState = () => {
-    setCount(count+1)
-  }
-    const increaseCountRef=()=>{
-      countRef.current = countRef.current+1
+    const doRendering = () => {
+        setRenderer(renderer+1)
+    }
+    const increaseRef = () => {
+        countRef.current = countRef.current + 1
+        console.log('ref: ', countRef.current)
     }
 
+    const increaseVar = () => {
+        countVar = countVar + 1
+        console.log('var: ', countVar)
+    }
 
-  return (
-   <div>
-     <p>State:{count}</p>
-       <p>Ref: {countRef.current} </p>
-     <button onClick={increaseCountState}>State 올려</button>
-       <button onClick={increaseCountRef}>Ref 올려</button>
-   </div>
-  );
+    const printResults = () => {
+        console.log(`ref: ${countRef.current}, var: ${countVar}`)
+    }
+
+    return (
+        <div>
+            <p>Ref: {countRef.current}</p>
+            <p>Var: {countVar}</p>
+            <button onClick={doRendering}>렌더!</button>
+            <button onClick={increaseRef}>Ref 올려</button>
+            <button onClick={increaseVar}>Var 올려</button>
+            <button onClick={printResults}>Ref Var 값 출력</button>
+        </div>
+    );
 }
 
 export default App;
